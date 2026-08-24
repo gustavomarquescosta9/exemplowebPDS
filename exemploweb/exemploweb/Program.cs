a36e5871bc2eff347c9e8243017b1b03cb11ed76
Gustavo Marques Costa <gustavomarquescosta9@gmail.com>
2026-08-24 09:35:19 -0400

    a

---
+++ exemploweb/exemploweb/Program.cs
@@ -0,0 +1,28 @@
+using exemploweb.Components;
+using exemploweb.Configs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+
+//Configuração da Conexão com o Banco de Dados MYSQLbuilder.Services.AddScoped<Conexao>();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+}
+app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
